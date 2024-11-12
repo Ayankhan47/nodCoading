f62083e42305c86f7ad736cf84a6301ed4f5fc09
Ayankhan47 <khanayan97553@gmail.com>
2024-11-12 23:48:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector("body").addEventListener("wheel", function(dets){
+    if(dets.deltaY > 0){
+        gsap.to("nav",{
+            duration : 0.25,
+            transform : `translateX(-50%) translateY(-150%)`
+        })
+    }else{
+        gsap.to("nav",{
+            duration : 0.25,
+            transform : `translateX(-50%) translateY(0%)`
+        })
+    }
+})
